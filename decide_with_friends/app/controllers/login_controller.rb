fb7665ce9df026e9b1fd2a775127094c39884db1
ffa7 <ffa7@sfu.ca>
2016-02-28 11:33:41 -0800

    returning polls for user invite

--- decide_with_friends/app/controllers/login_controller.rb
+++ decide_with_friends/app/controllers/login_controller.rb
@@ -15,9 +15,10 @@ class LoginController < ApplicationController
 
   def create 
     if !User.exists?(fb_id: params[:fb_id])
-      @newUser = User.new(params.permit(:first_name, :last_name, :email, :fb_id, :profile_picture))
-      @newUser.save
-      render json: @newUser
+      @user = User.new(params.permit(:first_name, :last_name, :email, :fb_id, :profile_picture))
+      @user.save
+      render :json => {:first_name => @user.first_name, :last_name => @user.last_name, :email => @user.email, :fb_id => @user.fb_id, :profile_picture => @user.profile_picture,
+          :polls => @user.polls}
     end
   end
 end
