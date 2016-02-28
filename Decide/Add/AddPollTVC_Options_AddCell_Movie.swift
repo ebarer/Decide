039e28f7acf5c189c79a98d0a588eecaa8305be2
Kahlan Gibson <kahlan.gibson@alumni.ubc.ca>
2016-02-28 02:06:26 -0800

    Adding Movie poll views

---
+++ Decide/Add/AddPollTVC_Options_AddCell_Movie.swift
@@ -0,0 +1,23 @@
+//
+//  AddPollTVC_Options_AddCell.swift
+//  Decide With Friends
+//
+//  Created by Elliot Barer on 2016-02-28.
+//  Copyright © 2016 Elliot Barer. All rights reserved.
+//
+
+import UIKit
+
+class AddPollTVC_Options_AddCell: UITableViewCell {
+
+    @IBOutlet var addOptionTextField: UITextField!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
---
+++ Decide/Add/PollTypeTVC_Movie.swift
@@ -0,0 +1,36 @@
+//
+//  PollTypeTVC.swift
+//  Decide With Friends
+//
+//  Created by Elliot Barer on 2016-02-27.
+//  Copyright © 2016 Elliot Barer. All rights reserved.
+//
+
+import UIKit
+
+class PollTypeTVC_Movie: UITableViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("Selected \(indexPath.row)")
+    }
+
+    
+    // MARK: - Navigation
+
+    @IBAction func cancelAddingPoll() {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
