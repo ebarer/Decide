2ec034d3fe079cf08f07af5f11cce74b52f42495
moults31 <moults31@gmail.com>
2016-02-28 01:28:09 -0800

    Add movie poll subclass of poll

--- Decide/Models/Polls/PollMovie.swift
+++ Decide/Models/Polls/PollMovie.swift
@@ -13,25 +13,26 @@ class MoviePoll: Poll {
     var latitude: Double = 0.0
     var longitude: Double = 0.0
     
-    convenience init(latitude: Double, longitude: Double) {
+    convenience init(title: String, latitude: Double, longitude: Double) {
+        self.init(title: title)
         self.latitude = latitude
         self.longitude = longitude
     }
 }
 
-class ViewWhereWeUseLocationStuff: CLLocationManagerDelegate {
-    var locationManager: CLLocationManager!
-    
-    func weAreInTheViewNow(){
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
-        locationManager.requestAlwaysAuthorization()
-        locationManager.startUpdatingLocation()
-        
-        //Keep this optional check, optional will have nil at first call otherwise and will throw error
-        if let latitude = locationManager.location?.coordinate.latitude {
-            //send lat and long to a movie poll
-        }
-    }
-    
-}
+//class ViewWhereWeUseLocationStuff: CLLocationManagerDelegate {
+//    var locationManager: CLLocationManager!
+//    
+//    func weAreInTheViewNow(){
+//        locationManager = CLLocationManager()
+//        locationManager.delegate = self
+//        locationManager.requestAlwaysAuthorization()
+//        locationManager.startUpdatingLocation()
+//        
+//        //Keep this optional check, optional will have nil at first call otherwise and will throw error
+//        if let latitude = locationManager.location?.coordinate.latitude {
+//            //send lat and long to a movie poll
+//        }
+//    }
+//    
+//}
