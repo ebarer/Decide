de45e4168332acad2370aabfc869b9f857f02e70
ffa7 <ffa7@sfu.ca>
2016-02-28 11:52:28 -0800

    returning options for polls

--- decide_with_friends/app/controllers/login_controller.rb
+++ decide_with_friends/app/controllers/login_controller.rb
@@ -6,8 +6,14 @@ class LoginController < ApplicationController
   def index
       if User.exists?(id: params[:pk_uid])
         @user = User.find(params[:pk_uid])
+        options ||= []
+        @user.polls.each do |poll|
+            @option = @poll.options
+            options << @option
+        end
+
         render :json => {:first_name => @user.first_name, :last_name => @user.last_name, :email => @user.email, :fb_id => @user.fb_id, :profile_picture => @user.profile_picture,
-          :polls => @user.polls}
+          :polls => @user.polls, :options => options}
       else
         render :json => '{"Error" : "-1", "Error_msg" : "User does not exist"}'
       end
