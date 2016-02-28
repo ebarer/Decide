597196ed29b96810992f2d583303eb020af4699d
ffa7 <ffa7@sfu.ca>
2016-02-27 22:15:09 -0800

    updated code for saving user

--- decide_with_friends/app/controllers/login_controller.rb
+++ decide_with_friends/app/controllers/login_controller.rb
@@ -8,14 +8,15 @@ class LoginController < ApplicationController
 
   def create 
     # Instantiate a new object using form parameters
-    @user = User.new(user_params)
+    @user = User.new(params.require(:user).permit(:first_name, :last_name, :email, :fb_id))
+    @user.save
     # Save the object
-    if @user.save
+    #if @user.save
     # If save succeeds, redirect to the index action
-      redirect_to(:action => 'index')
-    else
+      #redirect_to(:action => 'index')
+    #else
     # If save fails, redisplay the form so user can fix problems
-      redirect_to(:action => 'index')
+      #redirect_to(:action => 'index')
   	end 
   end
 end
